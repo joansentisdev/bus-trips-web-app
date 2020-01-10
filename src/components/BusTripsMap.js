9c5ae25ee78fb6facc297e1eecacec05183ef45d
Joan Sentis Personal <joansentispersonal@TW-DR-M-JG-2.local>
2020-01-10 17:43:10 +0100

    bus stops markers

--- src/components/BusTripsMap.js
+++ src/components/BusTripsMap.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { GoogleApiWrapper, Map, Polyline } from 'google-maps-react';
+import { GoogleApiWrapper, Map, Marker, Polyline } from 'google-maps-react';
 
 import mapStyles from '../utils/darkMapStyles';
+import busStopIcon from '../assets/icons/bus-stop.png';
 
 function BusTripsMap({ trip }) {
+  const [markers, setMarkers] = useState([]);
   const [mapOptions, setMapOptions] = useState({
     center: { lat: 0, lang: 0 },
     zoom: 3,
@@ -15,18 +17,35 @@ function BusTripsMap({ trip }) {
       return;
     }
 
+    // Re-center the map based on the route path
     const bounds = new window.google.maps.LatLngBounds();
     const polyline = getRoutePath();
-    polyline.forEach((item) => {
-      bounds.extend(item);
-    });
+    polyline.forEach((item) => bounds.extend(item));
     setMapOptions({
       center: bounds.getCenter(),
       zoom: 12,
     });
+
+    // Update the markers
+    setMarkers([
+      trip.origin.point,
+      trip.destination.point,
+      ...trip.stops.map(({ point }) => point).filter((stop) => !!stop),
+    ]);
   },
   // eslint-disable-next-line
   [trip]);
+
+  const renderPolyline = () => trip ? <Polyline path={getRoutePath()} strokeColor={'#3f51b5'} /> : null;
+  const renderMarkers = () => markers.map((marker, i) => <Marker key={i} position={{
+    lat: marker._latitude,
+    lng: marker._longitude,
+  }}
+  icon={{
+    url: busStopIcon,
+    anchor: new window.google.maps.Point(16,16),
+    scaledSize: new window.google.maps.Size(32,32)
+  }} />);
 
   return (
     <Map
@@ -36,9 +55,8 @@ function BusTripsMap({ trip }) {
       center={mapOptions.center}
       disableDefaultUI={true}
       zoomControl={true}>
-      {trip ? (
-        <Polyline path={getRoutePath()} />
-      ): null}
+      {renderPolyline()}
+      {renderMarkers()}
     </Map>
   );
 }
